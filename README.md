ab9b6fd2e986ed42731f72010bfb102828d9e8c4
Chris <chrismun96@gmail.com>
2018-08-27 11:36:09 -0400

    Version 3.0

--- README.md
+++ README.md
@@ -6,19 +6,21 @@ This is a compressor for The Legend of Zelda: Ocarina of Time. It takes a decomp
 
 This program will probably make your CPU fans run very loud, as it pins every core in your processor to 100% for a while. This is done to make it much faster than other compressors, at the cost of a bit of noise.
 
+With version 3.0, the compressor now makes a file called `ARCHIVE.bin`. This file is used to speed up subsequent compressoions by allowing the compressor to copy over similar files instead of compressing them again. This won't be too useful if you're only compressing vanilla ROMS (Well, it will compress in about a second, but I'm not sure why you would be compressing the same ROM over and over), but it should prove very useful if you're making ROM hacks or using the OoT Item Randomiser, because if you're only changing a few files, it won't compress the entire ROM again, and will instead only compress what it needs to.
+
 \*Results may vary
 
 ---
 
 Although you shouldn't need to compile, here's what you'll need to use to do so
 
-Compiling the compressor (Mac/Linux): gcc -g -O3 -o Compress.out compressor.c
+Compiling the compressor (Mac/Linux): gcc -s -O3 -o Compress.out compressor.c -lpthread
 
-Compiling the compressor (Windows): gcc -g -O3 -o Compress.exe compressor.c -lws2_32
+Compiling the compressor (Windows): gcc -s -O3 -o Compress.exe compressor.c -l:libpthread.a
 
-Compiling the table extractor (Mac/Linux): gcc -g -O3 -o TabExt.out tableExtractor.c
+Compiling the table extractor (Mac/Linux): gcc -s -O3 -o TabExt.out tableExtractor.c
 
-Compiling the table extractor (Windows): gcc -g -O3 -o TabExt.exe tableExtractor.c -lws2_32
+Compiling the table extractor (Windows): gcc -s -O3 -o TabExt.exe tableExtractor.c
 
 I got this to compile on Windows, but I needed a specific version of minGW to do it. I downloaded msys2 (https://www.msys2.org/) then used *pacman -S mingw-w64-x86_64-gcc* to get a version that could compile it, so hopefully that will work.
 
@@ -28,7 +30,7 @@ Compressor Usage: Compress.exe [Input ROM]
 
 Table Extractor usage: TabExt.exe [Input ROM]
 
-Compressor Notes: The compressor relies on a file called *table.bin* being in the same directory as the compressor executable. This file is created by the table extractor, so if you need one, just run that. The compressor will take whatever name you gave it as an argument, and add "-comp" to the end. So for example, if you gave it Zelda.z64, it would produce Zelda-comp.z64, which is the compressed ROM.
+Compressor Notes: The compressor relies on a file called `table.bin` being in the same directory as the compressor executable. This file is created by the table extractor, so if you need one, just run that. The compressor will take whatever name you gave it as an argument, and add `-comp` to the end. So for example, if you gave it Zelda.z64, it would produce Zelda-comp.z64, which is the compressed ROM.
 
 Table Extractor Notes: The table extractor takes a compressed ROM, and extracts the file table that's in the ROM for the compressor to use
 
--- compressor.c
+++ compressor.c
@@ -16,9 +16,9 @@
 typedef struct
 {
 	uint32_t startV;
-	uint32_t endV;
+	uint32_t   endV;
 	uint32_t startP;
-	uint32_t endP;	
+	uint32_t   endP;
 }
 table_t;
 
@@ -42,47 +42,103 @@ typedef struct
 }
 output_t;
 
+typedef struct
+{
+	uint32_t fileCount;
+	uint32_t* ref_size;
+	uint32_t* src_size;
+	uint8_t**      ref;
+	uint8_t**      src;
+}
+archive_t;
+
 /* Functions */
-uint32_t findTable();
+uint32_t findTable(uint8_t*);
 void getTableEnt(table_t*, uint32_t*, uint32_t);
 void* thread_func(void*);
 void errorCheck(int, char**);
+void makeArchive(char*, char*);
+int32_t getNumCores();
+int32_t getNext();
 
 /* Globals */
 uint8_t* inROM;
 uint8_t* outROM;
 uint8_t* refTab;
-uint32_t numThreads;
-pthread_mutex_t lock;
+char* name;
+pthread_mutex_t filelock;
+pthread_mutex_t countlock;
+int32_t numFiles, nextFile, arcCount;
+uint32_t* fileTab;
+archive_t* archive;
 output_t* out;
 
 int main(int argc, char** argv)
 {
 	FILE* file;
-	uint32_t* fileTab;
 	int32_t tabStart, tabSize, tabCount;
 	volatile int32_t prev, prev_comp;
-	int32_t i, size;
-	char* name;
-	args_t* args;
+	int32_t i, size, numCores, tempSize;
+	pthread_t* threads;
 	table_t tab;
-	pthread_t thread;
 
 	errorCheck(argc, argv);
+	printf("Starting compression.\n");
+	fflush(stdout);
 
 	/* Open input, read into inROM */
 	file = fopen(argv[1], "rb");
-	inROM = malloc(DCMPSIZE);
+	inROM = calloc(DCMPSIZE, sizeof(uint8_t));
 	fread(inROM, DCMPSIZE, 1, file);
 	fclose(file);
 
+	/* Read archive if it exists*/
+	file = fopen("ARCHIVE.bin", "rb");
+	if(file != NULL)
+	{
+		/* Get number of files */
+		printf("Loading Archive.\n");
+		fflush(stdout);
+		archive = malloc(sizeof(archive_t));
+		fread(&(archive->fileCount), sizeof(uint32_t), 1, file);
+
+		/* Allocate space for files and sizes */
+		archive->ref_size = malloc(sizeof(uint32_t) * archive->fileCount);
+		archive->src_size = malloc(sizeof(uint32_t) * archive->fileCount);
+		archive->ref = malloc(sizeof(uint8_t*) * archive->fileCount);
+		archive->src = malloc(sizeof(uint8_t*) * archive->fileCount);
+
+		/* Read in file size and then file data */
+		for(i = 0; i < archive->fileCount; i++)
+		{
+			/* Decompressed "Reference" file */
+			fread(&tempSize, sizeof(uint32_t), 1, file);
+			archive->ref[i] = malloc(tempSize);
+			archive->ref_size[i] = tempSize;
+			fread(archive->ref[i], 1, tempSize, file);
+
+			/* Compressed "Source" file */
+			fread(&tempSize, sizeof(uint32_t), 1, file);
+			archive->src[i] = malloc(tempSize);
+			archive->src_size[i] = tempSize;
+			fread(archive->src[i], 1, tempSize, file);
+		}
+		fclose(file);
+	}
+	else
+	{
+		printf("No archive found, this could take a while.\n");
+		fflush(stdout);
+		archive = NULL;
+	}
+
 	/* Find the file table and relevant info */
-	tabStart = findTable();
+	tabStart = findTable(inROM);
 	fileTab = (uint32_t*)(inROM + tabStart);
 	getTableEnt(&tab, fileTab, 2);
 	tabSize = tab.endV - tab.startV;
 	tabCount = tabSize / 16;
-	
+
 	/* Read in compression reference */
 	file = fopen("table.txt", "r");
 	fseek(file, 0, SEEK_END);
@@ -95,39 +151,47 @@ int main(int argc, char** argv)
 
 	/* Initialise some stuff */
 	out = malloc(sizeof(output_t) * tabCount);
-	pthread_mutex_init(&lock, NULL);
-	numThreads = 0;
+	pthread_mutex_init(&filelock, NULL);
+	pthread_mutex_init(&countlock, NULL);
+	numFiles = tabCount - 3;
+	nextFile = 3;
+	arcCount = 0;
+
+	/* Get CPU core count */
+	numCores = getNumCores() - 1;
+	threads = malloc(sizeof(pthread_t) * numCores);
+	printf("Detected %d cores\n", (numCores + 1));
+	fflush(stdout);
 
 	/* Create all the threads */
-	for(i = 3; i < tabCount; i++)
-	{
-		args = malloc(sizeof(args_t));
-
-		getTableEnt(&(args->tab), fileTab, i);
-		args->num = i;
-
-		pthread_mutex_lock(&lock);
-		numThreads++;
-		pthread_mutex_unlock(&lock);
-
-		pthread_create(&thread, NULL, thread_func, args);
-	}
+	for(i = 0; i < numCores; i++)
+		pthread_create(&threads[i], NULL, thread_func, NULL);
 
 	/* Wait for all of the threads to finish */
-	while(numThreads != 0)
-	{
-		printf("~%d threads remaining\n", numThreads);
-		fflush(stdout);
-		sleep(5);
-	}
+	for(i = 0; i < numCores; i++)
+		pthread_join(threads[i], NULL);
 
 	/* Setup for copying to outROM */
+	printf("Files compressed, writing new ROM.\n");
+	fflush(stdout);
 	outROM = calloc(COMPSIZE, sizeof(uint8_t));
 	memcpy(outROM, inROM, tabStart + tabSize);
-	pthread_mutex_destroy(&lock);
 	prev = tabStart + tabSize;
 	prev_comp = refTab[2];
 	tabStart += 0x20;
+
+	/* Free some stuff */
+	pthread_mutex_destroy(&filelock);
+	pthread_mutex_destroy(&countlock);
+	if(archive != NULL)
+	{
+		free(archive->ref);
+		free(archive->src);
+		free(archive->ref_size);
+		free(archive->src_size);
+		free(archive);
+	}
+	free(threads);
 	free(refTab);
 	free(inROM);
 
@@ -144,6 +208,7 @@ int main(int argc, char** argv)
 			tab.startP = prev;
 			if(out[i].comp)
 				tab.endP = tab.startP + size;
+
 			memcpy(outROM + tab.startP, out[i].data, size);
 			byteSwap(tab.startV, tab.startV);
 			byteSwap(tab.endV, tab.endV);
@@ -161,20 +226,6 @@ int main(int argc, char** argv)
 	free(out);
 
 	/* Make and fill the output ROM */
-	i = 0;
-	size = strlen(argv[1]);
-	name = malloc(size + 5);
-	strcpy(name, argv[1]);
-	while(i < size)
-	{
-		if(name[i] == '.')
-		{
-			name[i] = '\0';
-			break;
-		}
-		i++;
-	}
-	strcat(name, "-comp.z64");
 	file = fopen(name, "wb");
 	fwrite(outROM, COMPSIZE, 1, file);
 	fclose(file);
@@ -182,20 +233,29 @@ int main(int argc, char** argv)
 
 	/* Fix the CRC using some kind of magic or something */
 	fix_crc(name);
+
+	/* Make the archive if needed */
+	if(archive == NULL)
+	{
+		printf("Creating archive.\n");
+		fflush(stdout);
+		makeArchive(argv[1], name);
+	}
+
+	printf("Compression complete.\n");
+	fflush(stdout);
 	free(name);
-	
 	return(0);
 }
 
-uint32_t findTable()
+uint32_t findTable(uint8_t* argROM)
 {
 	uint32_t i, temp;
 	uint32_t* tempROM;
 
-	i = 0;
-	tempROM = (uint32_t*)inROM;
+	tempROM = (uint32_t*)argROM;
 
-	while(i+4 < UINTSIZE)
+	for(i = 0; i+4 < UINTSIZE; i += 4)
 	{
 		/* This marks the begining of the filetable */
 		byteSwap(temp, tempROM[i]);
@@ -219,8 +279,6 @@ uint32_t findTable()
 				}
 			}
 		}
-
-		i += 4;
 	}
 
 	fprintf(stderr, "Error: Couldn't find file table\n");
@@ -235,50 +293,202 @@ void getTableEnt(table_t* tab, uint32_t* files, uint32_t i)
 	byteSwap(tab->endP,   files[(i*4)+3]);
 }
 
-void* thread_func(void* arg)
+void* thread_func(void* null)
 {
 	args_t* a;
 	table_t t;
-	int i;
+	int32_t next, i, nextArchive;
 
-	a = arg;
-	t = a->tab;
-	i = a->num;
-
-	/* Setup the src*/
-	a->src_size = t.endV - t.startV;
-	a->src = inROM + t.startV;
-
-	/* If needed, compress and fix size */
-	/* Otherwise, just copy src into outROM */
-	if(refTab[i])
+	while((next = getNext()) != -1)
 	{
-		a->dst_size = a->src_size + 0x160;
-		a->dst = calloc(a->dst_size, sizeof(uint8_t));
-		yaz0_encode(a->src, a->src_size, a->dst, &(a->dst_size));
-		a->src_size = ((a->dst_size + 31) & -16);
-		out[i].comp = 1;
-		out[i].data = malloc(a->src_size);
-		memcpy(out[i].data, a->dst, a->src_size);
-		free(a->dst);
-	}
-	else
-	{
-		out[i].comp = 0;
-		out[i].data = malloc(a->src_size);
-		memcpy(out[i].data, a->src, a->src_size);
-	}
+		a = malloc(sizeof(args_t));
+		a->num = next;
+		getTableEnt(&(a->tab), fileTab, next);
+		t = a->tab;
+		i = a->num;
 
-	out[i].table = t;
-	out[i].size = a->src_size;
-	free(a);
+		/* Setup the src*/
+		a->src_size = t.endV - t.startV;
+		a->src = inROM + t.startV;
 
-	/* Decrement thread counter */
-	pthread_mutex_lock(&lock);
-	numThreads--;
-	pthread_mutex_unlock(&lock);
+		/* If needed, compress and fix size */
+		/* Otherwise, just copy src into outROM */
+		if(refTab[i])
+		{
+			pthread_mutex_lock(&countlock);
+			nextArchive = arcCount++;
+			pthread_mutex_unlock(&countlock);
+
+			/* If the uncompressed version is the same as vanilla, just copy/paste it */
+			/* Otherwise, compress it manually */
+			if((archive != NULL) && (memcmp(a->src, archive->ref[nextArchive], archive->ref_size[nextArchive]) == 0))
+			{
+				out[i].comp = 1;
+				a->src_size = archive->src_size[nextArchive];
+				out[i].data = malloc(a->src_size);
+				memcpy(out[i].data, archive->src[nextArchive], a->src_size);
+				free(archive->ref[nextArchive]);
+				free(archive->src[nextArchive]);
+			}
+			else
+			{
+				a->dst_size = a->src_size + 0x160;
+				a->dst = calloc(a->dst_size, sizeof(uint8_t));
+				yaz0_encode(a->src, a->src_size, a->dst, &(a->dst_size));
+				a->src_size = ((a->dst_size + 31) & -16);
+				out[i].comp = 1;
+				out[i].data = malloc(a->src_size);
+				memcpy(out[i].data, a->dst, a->src_size);
+				free(a->dst);
+				if(archive != NULL)
+				{
+					free(archive->ref[nextArchive]);
+					free(archive->src[nextArchive]);
+				}
+			}
+		}
+		else
+		{
+			out[i].comp = 0;
+			out[i].data = malloc(a->src_size);
+			memcpy(out[i].data, a->src, a->src_size);
+		}
+
+		/* Set up the table entry and size */
+		out[i].table = t;
+		out[i].size = a->src_size;
+		free(a);
+	}
 
 	return(NULL);
+}
+
+void makeArchive(char* fileOne, char* fileTwo)
+{
+	table_t tab;
+	uint32_t tabSize, tabCount, tabStart, i, fileCount;
+	uint32_t fileSize;
+	archive_t archive;
+	uint8_t* tempROM;
+	FILE* file;
+
+	/* Open and read the input files */
+	file = fopen(fileOne, "rb");
+	inROM = calloc(DCMPSIZE, sizeof(uint8_t));
+	fread(inROM, 1, DCMPSIZE, file);
+	fclose(file);
+
+	file = fopen(fileTwo, "rb");
+	tempROM = calloc(COMPSIZE, sizeof(uint8_t));
+	fread(tempROM, 1, COMPSIZE, file);
+	fclose(file);
+
+	/* Find some info on the DMAtable */
+	tabStart = findTable(tempROM);
+	fileTab = (uint32_t*)(tempROM + tabStart);
+	getTableEnt(&tab, fileTab, 2);
+	tabSize = tab.endV - tab.startV;
+	tabCount = tabSize / 16;
+	archive.fileCount = 0;
+	fileCount = 0;
+
+	/* Find the number of compressed files in the ROM */
+	for(i = 3; i <= tabCount; i++)
+	{
+		getTableEnt(&tab, fileTab, i);
+
+		if(tab.endP != 0)
+			fileCount++;
+	}
+
+	/* Open output file */
+	file = fopen("ARCHIVE.bin", "wb");
+	fwrite(&fileCount, sizeof(uint32_t), 1, file);
+
+	/* Write the archive data */
+	for(i = 3; i <= tabCount; i++)
+	{
+		getTableEnt(&tab, fileTab, i);
+
+		if(tab.endP != 0)
+		{
+			/* Write the size and data for the decompressed portion */
+			fileSize = tab.endV - tab.startV;
+			fwrite(&fileSize, sizeof(uint32_t), 1, file);
+			fwrite(inROM + tab.startV, 1, fileSize, file);
+
+			/* Write the size and data for the compressed portion */
+			fileSize = tab.endP - tab.startP;
+			fwrite(&fileSize, sizeof(uint32_t), 1, file);
+			fwrite((tempROM + tab.startP), 1, fileSize, file);
+		}
+	}
+
+	fclose(file);
+	free(tempROM);
+	free(inROM);
+}
+
+int32_t getNumCores()
+{
+	/* Windows */
+	#ifdef _WIN32
+
+		SYSTEM_INFO info;
+		GetSystemInfo(&info);
+		return(info.dwNumberOfProcessors);
+
+	/* Mac */
+	#elif MACOS
+
+		int nm[2];
+		size_t len;
+		uint32_t count;
+
+		len = 4;
+		nm[0] = CTL_HW;
+		nm[1] = HW_AVAILCPU;
+		sysctl(nm, 2, &count, &len, NULL, 0);
+
+		if (count < 1)
+		{
+			nm[1] = HW_NCPU;
+			sysctl(nm, 2, &count, &len, NULL, 0);
+			if (count < 1)
+				count = 1;
+		}
+		return(count);
+
+	/* Linux */
+	#else
+
+		return(sysconf(_SC_NPROCESSORS_ONLN));
+
+	#endif
+}
+
+int32_t getNext()
+{
+	int32_t file, temp;
+
+	/* Counter mutex */
+	pthread_mutex_lock(&filelock);
+	file = nextFile++;
+	pthread_mutex_unlock(&filelock);
+
+	/* Progress tracker */
+	if((file % 150) == 0)
+	{
+		temp = numFiles - file;
+		printf("%#4d files remaining\n", (temp + 2));
+		fflush(stdout);
+	}
+
+	/* Base case */
+	if (file > numFiles)
+		file = -1;
+
+	return(file);
 }
 
 void errorCheck(int argc, char** argv)
@@ -305,21 +515,42 @@ void errorCheck(int argc, char** argv)
 	/* Check that input ROM is correct size */
 	fseek(file, 0, SEEK_END);
 	i = ftell(file);
-	fclose(file);	
+	fclose(file);
 	if(i != DCMPSIZE)
 	{
 		fprintf(stderr, "Warning: Invalid input ROM size\n");
 		exit(1);
 	}
 
-	/* Check that table.bin exists */
-	file = fopen("table.txt", "r");
+    /* Check that table.txt exists */
+    file = fopen("table.txt", "r");
+    if(file == NULL)
+    {
+        perror("table.txt");
+        fclose(file);
+        exit(1);
+    }
+
+	/* Check that output ROM is writeable */
+	i = strlen(argv[1]) + 5;
+	name = malloc(i);
+	strcpy(name, argv[1]);
+	for(i; i > 0; i--)
+	{
+		if(name[i] == '.')
+		{
+			name[i] = '\0';
+			break;
+		}
+	}
+	strcat(name, "-comp.z64");
+	file = fopen(name, "wb");
 	if(file == NULL)
 	{
-		perror("table.txt");
-		fprintf(stderr, "If you do not have a table.txt file, please use TabExt first\n");
-		fclose(file);
-		exit(1);
+	    perror(name);
+	    fclose(file);
+	    free(name);
+	    exit(1);
 	}
 	fclose(file);
 }
