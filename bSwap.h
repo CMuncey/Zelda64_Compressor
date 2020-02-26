13855c7a77f7a8731bdfd0f51f8ddde95762bf4f
cmuncey <cmuncey@vols.utk.edu>
2020-02-25 19:02:43 -0500

    Accidentally reverted bSwap.h

--- bSwap.h
+++ bSwap.h
@@ -3,7 +3,17 @@
 
 #include <stdint.h>
 
-uint32_t bSwap32(uint32_t a)
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define bSwap32(x) _bSwap32(x)
+#define bSwap16(x) _bSwap16(x)
+
+#elif BYTE_ORDER == BIG_ENDIAN
+#define bSwap32(x) (x)
+#define bSwap16(x) (x)
+
+#endif
+
+uint32_t _bSwap32(uint32_t a)
 {
     return( (a & 0x000000FF) << 24 |
             (a & 0x0000FF00) <<  8 |
@@ -11,7 +21,7 @@ uint32_t bSwap32(uint32_t a)
             (a & 0xFF000000) >> 24 );
 }
 
-uint16_t bSwap16(uint16_t a)
+uint16_t _bSwap16(uint16_t a)
 {
     return( (a & 0x00FF) << 8 |
             (a & 0xFF00) >> 8 );
